9b6bcd037dbff42c201b369c3b0d9bc136aca6ec
Víctor Martínez <victorcoder2@gmail.com>
2020-12-25 00:20:15 +0100

    more devops

---
+++ Makefile
@@ -0,0 +1,8 @@
+watch:
+	docker-compose -f docker-compose-hot.yaml up
+
+watch-build:
+	docker-compose -f docker-compose-hot.yaml build
+
+watch-down:
+	docker-compose -f docker-compose-hot.yaml down
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod db_utils;
 use actix_web::{
     get,
     web::{self, Path},
-    App, HttpResponse, HttpServer, Responder,
+    App, HttpServer, Responder,
 };
 
 use db_utils::{get_pool, run_migrations};
@@ -16,7 +16,7 @@ use std::env;
 
 #[get("/{name}")]
 async fn greets(Path(name): Path<String>) -> impl Responder {
-    format!("Hello, {}!", name)
+    format!("Hello, how are you {}?", name)
 }
 
 #[actix_web::main]
@@ -24,7 +24,6 @@ async fn main() -> std::io::Result<()> {
     let db_url = env::var("PG_URL").expect("Error retrieving the database url");
     run_migrations(&db_url);
     let pool = get_pool(&db_url);
-
     HttpServer::new(move || App::new().service(greets).data(pool.clone()))
         .bind("0.0.0.0:4000")?
         .run()
